50f755adb129da3f2fa9ad3c2ed2cf8e1df4fa2e
Julian Luco Contestin <julianluco@MacBook-Pro-de-Julian.local>
2020-05-10 03:08:39 -0300

    se redisenia, se agrega secuencia de ejecucion usando await y funcion de restore de data

--- database/jLucoContestin.js
+++ database/jLucoContestin.js
@@ -1,147 +1,157 @@
-/*** 
- ** 
+/***
+ **
  ** DESAFIO 2 - DESARROLLO DE CRUD EN MONGODB
- ** 
+ **
  ****/
 
-const mongoClient = require('mongodb').MongoClient; 
-const uriDatabase = "mongodb+srv://odin:nido@cluster0-awpjk.mongodb.net/test?retryWrites=true&w=majority";
-const chalk = require('chalk');
-const client = new mongoClient(uriDatabase, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoClient = require("mongodb").MongoClient;
+const uriDatabase =
+  "mongodb+srv://odin:nido@cluster0-awpjk.mongodb.net/test?retryWrites=true&w=majority";
+const chalk = require("chalk");
+const client = new mongoClient(uriDatabase, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const createInventor = "insert" // inserto un objeto a la coleccion
-const readInventors = "showCollection" // leo los objetos de la coleccion y los muestro
-const updateInventor = "update" //actualizo un objeto de la coleccion
+const createInventor = "insert"; // inserto un objeto a la coleccion
+const readInventors = "showCollection"; // leo los objetos de la coleccion y los muestro
+const updateInventor = "update"; //actualizo un objeto de la coleccion
 const deleteInventor = "delete"; // elimino un objeto de la coleccion
 
-async function crud(operation, object, filter) {
-    return new Promise(() => {
-        client.connect((error, result) => {
-            if (!error) {
-                console.log(chalk.green("Connected correctly to server"));
-                const collectionInventors = result.db('desafio2').collection('inventores');
+async function openDb() {
+  return client.connect().then((result) => {
+    console.log(chalk.green("Connected successfully to server"));
+    return result.db("desafio2").collection("inventores");
+  });
+}
 
-                switch (operation) {
+async function closeDb() {
+    return client.close().then(() => console.log(chalk.green("Connection closed")))
+}
 
-                    case "insert":
-                        // CREATE - Inserto un nuevo objeto a la coleccion
-                        collectionInventors.insertOne(object, (error, result) => {
-                            if (!error) {
-                                console.log(chalk.blue("Documento insertado OK. \n"),chalk.italic.blueBright("Log: ",result,"\n"));
-                            } else {
-                                console.log(chalk.red("Error en CREATE. \n"),chalk.italic.redBright("Log: ",error,"\n"));
-                            }
-                        });
-                        break;
+async function crud(db, operation, object, filter) {
+  switch (operation) {
+    case "insert":
+      // CREATE - Inserto un nuevo objeto a la coleccion
+      return db.insertOne(object, (error, result) => {
+        if (!error) {
+          console.log(
+            chalk.blue("Documento insertado OK. \n"),
+            chalk.italic.blueBright("Log: ", result, "\n")
+          );
+        } else {
+          console.log(
+            chalk.red("Error en CREATE. \n"),
+            chalk.italic.redBright("Log: ", error, "\n")
+          );
+        }
+        return result;
+      });
 
-                    case "showCollection":
-                        // READ - Muestro los objetos/documentos de la coleccion
-                        collectionInventors.find({}).toArray((err, res)=> {
-                            if (!error) {
-                                    console.log(chalk.blue("\n"))
-                                    res.forEach(element => {
-                                        console.log(chalk.blue("Inventor: ",element.first),chalk.blue.bold(element.last),chalk.blue("(",element.year,")"))
-                                    });
-                                    console.log(chalk.blue("\n"))
-                            } else {
-                                console.log(chalk.red("Error en READ. \n"),chalk.italic.redBright("Log: ",error,"\n"));
-
-                            }
-                        });
-                        break;
-
-                    case "update":
-                        // UPDATE - Modifico un objeto existente de la coleccion
-                        collectionInventors.updateOne(filter, object, (error, result) => {
-                            if (!error) {
-                                console.log(chalk.blue("Documento actualizado OK. \n"),chalk.italic.blueBright("Log: ",result,"\n"));
-                            } else {
-                                console.log(chalk.red("Error en el UPDATE. \n"),chalk.italic.redBright("Log: ",error,"\n"));
-                            }
-                        });
-                        break;
-
-                    case "delete":
-                        // DELETE - Borro un nuevo objeto de la coleccion
-                        collectionInventors.deleteOne(object, (error, result) => {
-                            if (!error) {
-                                console.log(chalk.blue("Documento eliminado OK. \n"),chalk.italic.blueBright("Log: ",result,"\n"));
-                            } else {
-                                console.log(chalk.red("Error en el DELETE. \n"),chalk.italic.redBright("Log: ",error,"\n"));
-                            }
-                        });
-                        break;
-
-                    default:
-                        console.log(chalk.red("Error DEFAULT. \n"),chalk.italic.redBright("Log: ",error,"\n"));
-                        break;
-                }
-
-            } else {
-                console.log(chalk.red.bgWhite("\n",error,"\n"));
-            }
+    case "showCollection":
+      // READ - Muestro los objetos/documentos de la coleccion
+      return db
+        .find({})
+        .toArray()
+        .then((result) => {
+          console.log(chalk.blue("\n"));
+          console.log(chalk.blue("--- Coleccion de Inventores ---"));
+          result.forEach((element) => {
+            console.log(
+              chalk.blue("Inventor: ", element.first),
+              chalk.blue.bold(element.last),
+              chalk.blue("(", element.year, ")")
+            );
+          });
+          console.log(chalk.blue("\n"));
+          return result;
+        })
+        .catch((error) => {
+          console.log(
+            chalk.red("Error en READ. \n"),
+            chalk.italic.redBright("Log: ", error, "\n")
+          );
         });
-    client.close()
-    })
+
+    case "update":
+      // UPDATE - Modifico un objeto existente de la coleccion
+      return db.updateOne(filter, object, (error, result) => {
+        if (!error) {
+          console.log(
+            chalk.blue("Documento actualizado OK. \n"),
+            chalk.italic.blueBright("Log: ", result, "\n")
+          );
+        } else {
+          console.log(
+            chalk.red("Error en el UPDATE. \n"),
+            chalk.italic.redBright("Log: ", error, "\n")
+          );
+        }
+        return result;
+      });
+
+    case "delete":
+      // DELETE - Borro un nuevo objeto de la coleccion
+      return db.deleteOne(object, (error, result) => {
+        if (!error) {
+          console.log(
+            chalk.blue("Documento eliminado OK. \n"),
+            chalk.italic.blueBright("Log: ", result, "\n")
+          );
+        } else {
+          console.log(
+            chalk.red("Error en el DELETE. \n"),
+            chalk.italic.redBright("Log: ", error, "\n")
+          );
+        }
+        return result;
+      });
+
+    default:
+      console.log(
+        chalk.red("Error DEFAULT. \n"),
+        chalk.italic.redBright("Log: ", error, "\n")
+      );
+      return null;
+  }
 }
 
-async function executeCrud(operation, object, filter) {
-    console.log(chalk.yellow("Connecting..."))
-    const resultado = await crud(operation, object, filter)
-    console.log(resultado)
-    client.close()
+async function executeCrud() {
+  console.log(chalk.yellow("Connecting..."));
+  const db = await openDb();
+  const read1 = await crud(db, readInventors) // READ
+  const inventor = { first: "Homero", last: "Simpson", year: 1956 }; // Objeto que voy a insertar
+  const create1 = await crud(db, createInventor, inventor) // CREATE
+  const updateObj = { first: "Leonardo", last: "Vinci" }; // Objeto a modificar (select)
+  const newData = { $set: { last: "Da Vinci", year: 1452 } }; // Datos a setear
+  const update1 = await crud(db, updateInventor, newData, updateObj) // UPDATE
+  const vendeHumo = { last: "Ponzi" }; // Objeto a borrar (select)
+  const delete1 = await crud(db, deleteInventor, vendeHumo); // DELETE
+  const read2 = await crud(db, readInventors); // READ
+  const dbc = await closeDb()
 }
 
-/**
-**
-** SET DE PRUEBAS | SET DE PRUEBAS | SET DE PRUEBAS | SET DE PRUEBAS --->
-**
-***/
-
-
-/* 
-* PASO 1 - MUESTRO LOS DATOS EN LA COLECCION 
-**/
-
-    //-> decomentar siguiente linea para probar
-    //  executeCrud(readInventors);
-
-
-/* 
-* PASO 2 - INSERTO UN OBJETO 
-**/
-
-    const inventor = { first: "Homero", last: "Simpson", year: 1956 }; // Objeto que voy a insertar
-
-    //-> decomentar siguiente linea para probar
-    // executeCrud(createInventor, inventor);
+executeCrud();
 
 
 /*
-* PASO 3 - MODIFICO UN OBJETO 
-**/
+ * RESTORE DE DATOS
+ * Utilizar solo para restaurar a los datos iniciales para hacer el test del CRUD.
+ * Una vez ejecutada la prueba anterior comentar executeCrud() y ejecutar dataRestore() para restablecer la BD
+ **/
 
-    const updateObj = { "first": "Leonardo", "last": "La Vinci" }; // Objeto a modificar (select)
-    const newData = { $set: { "last": "Da Vinci", "year": 1452 } }; // Datos a setear
+// async function dataRestore() {
+//   console.log(chalk.yellow("Connecting..."));
+//   const db = await openDb();
+//   const ponzi = { first: "Carlo", last: "Ponzi", year: 1882 }; // Objeto ponzi
+//   const create2 = await crud(db, createInventor, ponzi) // CREATE ponzi
+//   const leonardo = { "first": "Leonardo" }; // select leonardo
+//   const fakeData = { $set: { "last": "Vinci", "year": 1952 } }; // datos incorrectos
+//   const update2 = await crud(db, updateInventor, fakeData, leonardo) // UPDATE leonardo
+//   const homero = { last: "Simpson" }; // select homero
+//   const delete2 = await crud(db, deleteInventor, homero); // DELETE
+//   const read3 = await crud(db, readInventors); // READ
+//   const dbc = await closeDb()
+// }
 
-    //-> decomentar siguiente linea para probar
-    // executeCrud(updateInventor, newData, updateObj);
-
-
-/* 
-* PASO 4 - BORRO UN OBJETO 
-**/
-
-    const deleteVendeHumo = { "first": "Marcelo", "last": "Tinelli",};
-
-    //-> decomentar siguiente linea para probar
-    // executeCrud(deleteInventor, deleteVendeHumo);
-
-
-/* 
-* PASO 5 - MUESTRO LOS DATOS DE LA COLECCION DESPUES DE EJECUTAR CREATE/UPDATE/DELETE 
-**/
-
-    //-> decomentar siguiente linea para probar
-    //  executeCrud(readInventors);
-
+// dataRestore();
